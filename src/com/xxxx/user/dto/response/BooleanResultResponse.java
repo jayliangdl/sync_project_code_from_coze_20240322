902124627ed80c9070c2e0be85205b64631ac988
lianghaojin <33975063+jayliangdl@users.noreply.github.com>
2024-03-23 13:21:37 +0800

    Update file

---
+++ src/com/xxxx/user/dto/response/BooleanResultResponse.java
@@ -0,0 +1,8 @@
+// Here is the content of BooleanResultResponse.java
+
+package com.xxxx.user.dto.response;
+
+public class BooleanResultResponse {
+
+    // Fields for boolean result response go here
+}
