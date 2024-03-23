6b4941787e10171c4941cac417cefbc3c04a3396
lianghaojin <33975063+jayliangdl@users.noreply.github.com>
2024-03-23 13:19:57 +0800

    Update file

---
+++ src/com/xxxx/user/dao/UcUserDao.java
@@ -0,0 +1,12 @@
+// Here is the content of UcUserDao.java
+
+package com.xxxx.user.dao;
+
+import com.xxxx.user.dto.AddUserRequest;
+import com.xxxx.user.dto.DeleteUserRequest;
+import com.xxxx.user.dto.UpdateUserPasswordRequest;
+
+public interface UcUserDao {
+
+    // Methods for user operations (add, delete, update password) go here
+}
