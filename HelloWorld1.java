25734dd909def4bbd793bf138c8ec0fd64538fc2
lianghaojin <33975063+jayliangdl@users.noreply.github.com>
2024-03-23 10:00:17 +0800

    Update file

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
+public class HelloBeijing {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Beijing!");
     }
 }
