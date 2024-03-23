7e1bdb2162d56df0a5d08f4d5962fca2427f522b
lianghaojin <33975063+jayliangdl@users.noreply.github.com>
2024-03-23 11:51:20 +0800

    Update file

---
+++ src/com/xxxx/commerce/market/vo/DeleteUserRequest.java
@@ -0,0 +1,14 @@
+package com.xxxx.commerce.market.vo;
+
+public class DeleteUserRequest {
+
+    private String userId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
