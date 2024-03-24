4f7b73344bddd47c44c40427e36ea2bf22c2b5ac
lianghaojin <33975063+jayliangdl@users.noreply.github.com>
2024-03-24 08:25:29 +0800

    Update file

--- src/com/xxxx/user/model/UserDO.java
+++ src/com/xxxx/user/model/UserDO.java
@@ -1,45 +1,23 @@
 package com.xxxx.user.model;
 
 import lombok.Data;
-import java.sql.Timestamp;
 
-/**
-* 和uc_user数据表对应，用户的数据对象模型
-*/
+
 @Data
 public class UserDO {
-    /**
-    * ID
-    */
+
     private Integer id;
 
-    /**
-    * 用户名字
-    */
     private String userName;
 
-    /**
-    * 性别 "1"-男 "2"-女
-    */
-    private char gender;
+    private String gender;
 
-    /**
-    * 拼团状态 "1"-有效 "2"-失效
-    */
-    private char status;
+    private String status;
 
-    /**
-    * 加密后的用户密码信息
-    */
     private String psw;
 
-    /**
-    * 记录创建时间
-    */
-    private Timestamp createDatetime;
+    private Date createDatetime;
 
-    /**
-    * 记录最后更新时间
-    */
-    private Timestamp updateDatetime;
+    private Date updateDatetime;
+
 }
