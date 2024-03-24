f5c779fce80559594fba1c3dc68844f29f127e97
lianghaojin <33975063+jayliangdl@users.noreply.github.com>
2024-03-24 08:38:04 +0800

    Update file

--- src/com/xxxx/user/model/UserDO.java
+++ src/com/xxxx/user/model/UserDO.java
@@ -3,28 +3,30 @@ package com.xxxx.user.model;
 import lombok.Data;
 import java.util.Date;
 
+/**
+ * 用户领域对象
+ */
 @Data
-/** "UserDO" 数据对象类：这个类对应数据库表 "uc_user"，这个类包含所有字段，并且可以被用来进行数据传输以及映射。 */
 public class UserDO {
 
-    /** ID 字段： 这是数据库表的ID字段，自动递增，唯一键 */
+    /** ID,自增,唯一键 */
     private Integer id;
 
-    /** userName 字段：这是“用户名称”，不能为空 */
+    /** 用户名字,非空 */
     private String userName;
 
-    /** gender 字段： 这是“性别”，不能为空，1-男，2-女 */
+    /** 性别,非空  "1"-男 "2"-女 */
     private String gender;
 
-    /** status 字段： 这是“拼团状态”，可以有多个枚举值（1-有效，2-失效） */
+    /** 拼团状态,枚举值 "1"-有效 "2"-失效 */
     private String status;
 
-    /** psw 字段： 这是“加密后的用户密码信息”，非空 */
+    /** 加密后的用户密码信息,非空 */
     private String psw;
 
-    /** createDatetime 字段： 这是记录的创建时间，默认值是NOW() */
+    /** 记录创建时间,now() */
     private Date createDatetime;
 
-    /** updateDatetime 字段： 这是记录最后更新的时间，默认值是NOW() */
+    /** 记录最后更新时间,now() */
     private Date updateDatetime;
 }
