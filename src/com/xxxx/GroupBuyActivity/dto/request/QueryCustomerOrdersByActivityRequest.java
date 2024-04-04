27d4cdd5844e9e5526c25b7606e7492ad753e5c0
lianghaojin <33975063+jayliangdl@users.noreply.github.com>
2024-04-04 22:12:02 +0800

    Update file

--- src/com/xxxx/GroupBuyActivity/dto/request/QueryCustomerOrdersByActivityRequest.java
+++ src/com/xxxx/GroupBuyActivity/dto/request/QueryCustomerOrdersByActivityRequest.java
@@ -3,6 +3,7 @@ package com.xxxx.GroupBuyActivity.dto.request;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.util.List;
 
@@ -23,6 +24,7 @@ public class QueryCustomerOrdersByActivityRequest {
     private String sortField;
 
     @NotEmpty(message = "结果排序要求不能为空")
+    @Pattern(regexp = "^(由大到小|由小到大)$", message = "结果排序要求应为'由大到小'或'由小到大'")
     private String sortOrder;
 
     @NotEmpty(message = "订单下单时间范围-开始日期时间不能为空")
