2eb51a63e2bd766a59db6e6ece163d0a4e674072
lianghaojin <33975063+jayliangdl@users.noreply.github.com>
2024-04-04 22:07:30 +0800

    Update file

---
+++ src/com/xxxx/GroupBuyActivity/dto/response/PlaceOrderResponse.java
@@ -0,0 +1,15 @@
+package com.xxxx.GroupBuyActivity.dto.response;
+
+import lombok.Data;
+
+@Data
+public class PlaceOrderResponse {
+    private boolean success;
+    private ExceptionInfo exceptionInfo;
+
+    @Data
+    public static class ExceptionInfo {
+        private String exceptionCode;
+        private String exceptionMessage;
+    }
+}
