1507340f380d42ad6bca3bf5ebf7bf4904044dc5
lianghaojin <33975063+jayliangdl@users.noreply.github.com>
2024-04-04 22:08:45 +0800

    Update file

--- src/com/xxxx/GroupBuyActivity/dto/request/QueryCustomerOrdersByActivityRequest.java
+++ src/com/xxxx/GroupBuyActivity/dto/request/QueryCustomerOrdersByActivityRequest.java
@@ -1,47 +1,33 @@
 package com.xxxx.GroupBuyActivity.dto.request;
 
 import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class QueryCustomerOrdersByActivityRequest {
-    /**
-     * 活动编号
-     */
+    @NotEmpty(message = "活动编号不能为空")
     private String activityId;
-    
-    /**
-     * 顾客编号
-     */
+
+    @NotEmpty(message = "顾客编号不能为空")
+    @Size(max = 16, message = "顾客编号长度不能超过16位")
     private String customerId;
-    
-    /**
-     * 订单状态条件列表
-     */
+
     private List<String> orderStatusList;
-    
-    /**
-     * 订单号
-     */
+
     private String orderId;
-    
-    /**
-     * 结果排序字段
-     */
+
+    @NotEmpty(message = "结果排序字段不能为空")
     private String sortField;
-    
-    /**
-     * 结果排序要求
-     */
+
+    @NotEmpty(message = "结果排序要求不能为空")
     private String sortOrder;
-    
-    /**
-     * 订单下单时间范围-开始日期时间
-     */
+
+    @NotEmpty(message = "订单下单时间范围-开始日期时间不能为空")
     private String startTime;
-    
-    /**
-     * 订单下单时间范围-结束日期时间
-     */
+
+    @NotEmpty(message = "订单下单时间范围-结束日期时间不能为空")
     private String endTime;
 }
